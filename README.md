5712ec5400a7e5f38d6253058eb5cd517ed9809f
BELHAJ Manel <77749468+belhajManel@users.noreply.github.com>
2021-12-12 18:11:17 +0000

    new

--- README.md
+++
@@ -1,2 +0,0 @@
-# Flutter-App-for-pfe-report-management
-Building Flutter UI for pfe reports management connected with SpringBoot  Rest API
--- android/app/src/main/kotlin/com/example/pfe_managment/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pfe_managment
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
